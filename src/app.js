f6c8042d8ef5cae52148554f1e08e325298863e3
saswata <saschak07@gmail.com>
2020-08-29 20:46:02 +0530

    adding cors

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ require('./db/mongoose')
 const UserRouter = require('./router/user')
 const HackerRouter = require('./router/hacker')
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(UserRouter)
 app.use(HackerRouter)
 
